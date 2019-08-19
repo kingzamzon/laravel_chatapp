636a32a503a3633187077d62c318ce1dafe74f30
Ade Niyi <adesanoye.samson@yahoo.com>
2019-08-19 14:41:23 -0700

    make authentication of user and rote to login

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,8 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'Auth\LoginController@showLoginForm');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
