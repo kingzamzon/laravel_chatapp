e5b0ec84049ec9a6c342a5133a129eb510b66dac
Ade Niyi <adesanoye.samson@yahoo.com>
2019-08-19 14:12:32 -0700

    create the ui for the chat

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,95 +5,62 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Laravel</title>
-
+        <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css')}}">
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
         <!-- Styles -->
         <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
+        [chat-box] {
+            width: 5000px;
+            display: inline-block;
+        }
+        @media screen and (min-width: 480px) {
+            [chat-box] {
+            width: 400px;
+            display: inline-block;
             }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
+        }
         </style>
     </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
+    <body style="background-color:lavender;">
+    <section class="container" style="background-color:white;margin-top: 20px;">
+            <div class="row" >
+                    <div class="col-sm-12" style="height: 300px;">
+                        <h4 style="border-bottom: 1px solid black">Chats</h4>
+                        <p chat-content></p>
+                    </div>
+                    <div class="col-sm-12 bg-secondary" >
+                        
+                        <form role="form" class="form-group" style="margin-top: 20px">
+                            {{csrf_field()}}
+                            <div class="input-group">
+                              <input type="text" name="message" chat-box class="form-control" placeholder="Type...">
+                              <div class="input-group-prepend">
+                                <button type="submit" class="input-group-text">Send</button>
+                              </div>
+                            </div> 
+                          </form>
+                    </div>
                 </div>
-            @endif
+    </section>
+    
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
-        </div>
-    </body>
+    <script>
+        const chats = [];
+        const chatField = document.querySelector('[chat-box]');
+        chatField.addEventListener("keydown", (e) => {
+            // console.log(e.key);
+            if(e.key == 'Enter') {
+            const {value} = chatField;
+            const currentDate = new Date();
+            chats.push({name: value, date: currentDate});
+            console.log(value);
+            console.log(chats);
+            }
+                
+        })
+        console.log(chatField);
+       
+    </script>
 </html>
