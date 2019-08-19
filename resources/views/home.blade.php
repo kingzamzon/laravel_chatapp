45d99c7982ab44eb161687be9e7d87398b9c4c9d
Ade Niyi <adesanoye.samson@yahoo.com>
2019-08-19 15:01:14 -0700

    A resource messagescontroller and edit chatbox

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">Chat</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -13,7 +13,7 @@
                             {{ session('status') }}
                         </div>
                     @endif
-                    <div>
+                    <div style="height:200px">
                         You are logged in!
                     </div>
                     
