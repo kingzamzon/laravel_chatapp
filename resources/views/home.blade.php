c5ba87e9f8fc290a529e2e7c7bd6bea18c26e5e6
Ade Niyi <adesanoye.samson@yahoo.com>
2019-08-21 14:15:36 -0700

    include sidebar and removed unwanted codes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,44 +3,15 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-4">
+            @include('sidebar',['user_messages'=>$user_messages])
+        </div> 
+        <div class="col-md-8"> 
             <div class="card">
                 <div class="card-header">Chat</div>
 
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-                    <div style="max-height:300px;height:300px;overflow-y: scroll;">
-                        @if (count($conversations) > 0)
-                        @foreach ($conversations as $c)
-                            <div class="card">
-                                <div class="card-body">
-                                <b>{{$c->user->name}}</b> <br>
-                                {{$c->message}}
-                                </div>
-                            </div><br>
-                            
-                        @endforeach
-                        @else 
-                            <p>No conversation so far. Start a conversation</p>
-                        @endif
-                                 </div>
-                    
-                    <div>
-                        <form role="form" class="form-group" method="POST" action="{{action('MessagesController@store')}}" style="margin-top: 20px">
-                            {{csrf_field()}}
-                            <!-- <input type="hidden" name="_method" value="PUT"> -->
-                            <div class="input-group">
-                              <input type="text" name="message" autocomplete="off" chat-box class="form-control" placeholder="Type...">
-                              <div class="input-group-prepend">
-                                <button type="submit" class="input-group-text">Send</button>
-                              </div>
-                            </div> 
-                          </form>
-                    </div>
+                    Continue chat or start a new Conversation
                 </div>
             </div>
         </div>
