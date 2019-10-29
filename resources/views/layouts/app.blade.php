1347387e8ba40354f0b56976e1b0347d67cc4723
Ade Niyi <adesanoye.samson@yahoo.com>
2019-10-28 17:55:50 -0700

    added jquery

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,9 +12,6 @@
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
 
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
@@ -71,10 +68,13 @@
                 </div>
             </div>
         </nav>
-
+        <script type="text/javascript" src="{!! asset('jquery-3.2.1.min.js') !!}"></script>
+        
         <main class="py-4">
             @yield('content')
+          
         </main>
     </div>
+
 </body>
 </html>
